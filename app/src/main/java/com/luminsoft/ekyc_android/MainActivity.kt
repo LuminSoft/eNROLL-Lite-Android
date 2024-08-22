cdc3193d0b6180e0132a4237a40292d952c7b030
luminsoft <andrew_samir93@hotmail.com>
2024-08-22 14:18:39 +0300

    targetSdk 51

--- app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
+++ app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
@@ -55,15 +55,15 @@ import io.github.cdimascio.dotenv.dotenv
 
 var dotenv = dotenv {
     directory = "/assets"
-//   filename = "env_andrew"
+//    filename = "env_andrew"
 //    filename = "env_radwan"
-    filename = "env_org_1"
+//    filename = "env_org_1"
 //    filename = "env_support_team"
 //    filename = "env_org2"
 //    filename = "env_azimut_production"
 //    filename = "env_lumin_production"
 //    filename = "env_naspas_production"
-
+    filename = "env_test_2"
 }
 
 var tenantId = mutableStateOf(TextFieldValue(text = dotenv["TENANT_ID"]))
