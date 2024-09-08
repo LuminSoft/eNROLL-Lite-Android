6f5e98963a742b7ee39fffce17a76e50e009743e
luminsoft <andrew_samir93@hotmail.com>
2024-09-08 11:19:53 +0300

    versionCode 64

--- app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
+++ app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
@@ -62,7 +62,7 @@ import io.github.cdimascio.dotenv.dotenv
 var dotenv = dotenv {
     directory = "/assets"
 //    filename = "env_andrew"
-    filename = "env_radwan"
+//    filename = "env_radwan"
 //    filename = "env_org_1"
 //    filename = "env_support_team"
 //    filename = "env_org2"
@@ -71,7 +71,7 @@ var dotenv = dotenv {
 //    filename = "env_naspas_production"
 //    filename = "env_naspas_staging"
 //    filename = "env_fra_staging"
-//    filename = "env_test_2"
+    filename = "env_test_2"
 }
 
 var tenantId = mutableStateOf(TextFieldValue(text = dotenv["TENANT_ID"]))
