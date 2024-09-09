d6431763facb33d67312b0e387016153de8d2ecc
Ahmed Radwan <Radwan@Ahmeds-MacBook-Pro.local>
2024-09-09 12:19:58 +0300

    changing the splash image

--- eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/main_auth/main_auth_presentation/common/SplashAuthScreenContent.kt
+++ eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/main_auth/main_auth_presentation/common/SplashAuthScreenContent.kt
@@ -2,14 +2,20 @@ package com.luminsoft.enroll_sdk.main_auth.main_auth_presentation.common
 
 import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -18,8 +24,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import appColors
 import com.luminsoft.ekyc_android_sdk.R
 import com.luminsoft.enroll_sdk.core.failures.AuthFailure
 import com.luminsoft.enroll_sdk.core.models.EnrollFailedModel
@@ -28,6 +37,7 @@ import com.luminsoft.enroll_sdk.main.main_presentation.common.ComposeLottieAnima
 import com.luminsoft.enroll_sdk.main_auth.main_auth_presentation.main_auth.view_model.AuthViewModel
 import com.luminsoft.enroll_sdk.ui_components.components.BottomSheetStatus
 import com.luminsoft.enroll_sdk.ui_components.components.DialogView
+import com.luminsoft.enroll_sdk.ui_components.components.ScreenHelper
 
 
 @Composable
@@ -35,33 +45,63 @@ fun SplashScreenAuthContent(
     viewModel: AuthViewModel,
     navController: NavController,
 ) {
-//    val loading = viewModel.loading.collectAsState()
     val failure = viewModel.failure.collectAsState()
-//    val steps = viewModel.steps.collectAsState()
-//    val context = LocalContext.current
     val activity = LocalContext.current as Activity
     viewModel.navController = navController
 
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Image(
-                painterResource(R.drawable.splash_screen),
-                contentDescription = "",
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier
-                    .fillMaxSize()
-            )
+    Surface(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.appColors.backGround),) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.appColors.backGround),) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier
                     .fillMaxSize()
             ) {
-                Spacer(modifier = Modifier.height(100.dp))
+                Image(
+                    painterResource(R.drawable.logo),
+                    contentScale = ContentScale.FillBounds,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .width(ScreenHelper.sw(0.46))
+                        .height(ScreenHelper.sh(0.12)),
+                )
                 ComposeLottieAnimation(
                     modifier = Modifier
                         .size(150.dp)
                 )
+
+
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter) // Align Row at the bottom
+                    .padding(bottom = 72.dp) // Add padding if needed
+            ) {
+                Text(
+                    text = "Sponsored by",
+                    color = MaterialTheme.appColors.textColor,
+                    textAlign = TextAlign.Center,
+                    fontSize = 12.sp
+                )
+
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Image(
+                    painter = painterResource(R.drawable.horizontal_footer),
+                    contentScale = ContentScale.FillBounds,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .width(ScreenHelper.sw(0.15)) // Adjust the width as needed
+                        .height(ScreenHelper.sh(0.05))
+                )
+
             }
         }
     }
