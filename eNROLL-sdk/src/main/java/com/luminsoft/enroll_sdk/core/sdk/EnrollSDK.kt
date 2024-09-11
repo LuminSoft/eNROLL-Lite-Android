4ba6d0bca8d5eec6fea7f4ab3e6b6ad234d7b81d
luminsoft <andrew_samir93@hotmail.com>
2024-09-11 17:07:44 +0300

    versionCode 65

--- eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/core/sdk/EnrollSDK.kt
+++ eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/core/sdk/EnrollSDK.kt
@@ -28,8 +28,8 @@ object EnrollSDK {
 
     private fun getBaseUrl(): String {
         return when (environment) {
-//            EnrollEnvironment.STAGING -> "http://197.44.231.206"
-            EnrollEnvironment.STAGING -> "http://197.168.1.39"
+            EnrollEnvironment.STAGING -> "http://197.44.231.206"
+//            EnrollEnvironment.STAGING -> "http://197.168.1.39"
             EnrollEnvironment.PRODUCTION -> "https://enroll.nasps.org.eg"
         }
     }
