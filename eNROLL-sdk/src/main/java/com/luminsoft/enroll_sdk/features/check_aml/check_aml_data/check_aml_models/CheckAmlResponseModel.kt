0323fd92ec23f36e74c67fe764f30e0c4a9c3c89
luminsoft <andrew_samir93@hotmail.com>
2024-10-07 14:31:50 +0300

    versionCode 77

--- eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/features/check_aml/check_aml_data/check_aml_models/CheckAmlResponseModel.kt
+++ eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/features/check_aml/check_aml_data/check_aml_models/CheckAmlResponseModel.kt
@@ -3,6 +3,9 @@ package com.luminsoft.enroll_sdk.features.check_aml.check_aml_data.check_aml_mod
 import com.google.gson.annotations.SerializedName
 
 
-data class CheckAmlResponseModel (
-    @SerializedName("status") var isWhiteListed: Boolean? = null
+data class CheckAmlResponseModel(
+    @SerializedName(
+        value = "status",
+        alternate = ["isWhiteListed"]
+    ) var isWhiteListed: Boolean? = null
 )
