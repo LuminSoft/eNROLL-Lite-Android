2db02047364b4d666fad8d35d93da68ed385f06f
luminsoft <andrew_samir93@hotmail.com>
2024-11-19 15:00:36 +0200

    add orange staging organization

--- app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
+++ app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
@@ -62,16 +62,17 @@ import io.github.cdimascio.dotenv.dotenv
 var dotenv = dotenv {
     directory = "/assets"
 //    filename = "env_andrew"
-    filename = "env_radwan"
+//    filename = "env_radwan"
 //    filename = "env_org_1"
 //    filename = "env_support_team"
 //    filename = "env_org2"
 //    filename = "env_azimut_production"
-    filename = "env_lumin_production"
+//    filename = "env_lumin_production"
 //    filename = "env_naspas_production"
 //    filename = "env_naspas_staging"
 //    filename = "env_fra_staging"
 //    filename = "env_test_2"
+    filename = "env_orange_staging"
 }
 
 var tenantId = mutableStateOf(TextFieldValue(text = dotenv["TENANT_ID"]))
