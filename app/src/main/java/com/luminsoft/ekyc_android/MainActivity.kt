878565f262bb1a71ffa2d175caab3a60126388ae
luminsoft <andrew_samir93@hotmail.com>
2025-04-16 13:55:42 +0200

    innovatrics version 8.6.1

--- app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
+++ app/src/main/java/com/luminsoft/ekyc_android/MainActivity.kt
@@ -62,9 +62,9 @@ import java.io.File
 
 var dotenv = dotenv {
     directory = "/assets"
-//    filename = "env_andrew"
+    filename = "env_andrew"
 //    filename = "env_radwan"
-    filename = "env_org_1"
+//    filename = "env_org_1"
 //    filename = "env_support_team"
 //    filename = "env_org2"
 //    filename = "env_azimut_production"
